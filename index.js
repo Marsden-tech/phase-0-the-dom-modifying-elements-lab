cb33c5dec4d1e0417adf3c9a6a25ec366022e8e5
Marsden Mathews Odhiambo <marsdenlwero@gmail.com>
2024-07-31 20:53:37 +0300

    completes lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+const mainElement = document.querySelector('main#main');
+
+if (mainElement) {
+    mainElement.parentNode.removeChild(mainElement);
+}
+const newHeader = document.createElement('h1');
+newHeader.id = "victory";
+newHeader.innerHTML = "Mazo is the champion";
